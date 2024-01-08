ae415fffc06ca7d6eac39a8ff872c180958fc4c8
PakrinhaCBNU <150335078+PakrinhaCBNU@users.noreply.github.com>
2024-01-08 06:21:17 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,55 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+const handleEvent = (type, data) => {
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    if (post) {
+      post.comments.push({ id, content, status });
+    }
+  }
+
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    const comment = post.comments.find((c) => c.id === id);
+    if (comment) {
+      comment.status = status;
+      comment.content = content;
+    }
+  }
+};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
+  console.log(posts);
+  res.send({});
+});
+
+app.listen(4002, async () => {
+  console.log('Listening on 4002');
+  const res = await axios.get('http://event-bus-srv:4005/events');
+  for (const event of res.data) {
+    handleEvent(event.type, event.data);
+  }
+});
