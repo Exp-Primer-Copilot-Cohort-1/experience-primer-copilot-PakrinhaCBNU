f446b23cbd9b5f398f1f0cb44257b6b8ff601a87
PakrinhaCBNU <150335078+PakrinhaCBNU@users.noreply.github.com>
2024-01-08 06:17:08 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,6 +1,14 @@
 function skillsMember() {
-  return {
-    restrict: 'E',
-    templateUrl: 'templates/skills-member.html'
+  const member = document.querySelector('.member');
+  if (member) {
+    const memberSkills = document.querySelector('.member__skills');
+    const memberSkillsList = document.querySelector('.member__skills-list');
+    const memberSkillsBtn = document.querySelector('.member__skills-btn');
+
+    memberSkillsBtn.addEventListener('click', () => {
+      memberSkills.classList.toggle('member__skills--active');
+      memberSkillsBtn.classList.toggle('member__skills-btn--active');
+      memberSkillsList.classList.toggle('member__skills-list--active');
+    });
   }
 }
